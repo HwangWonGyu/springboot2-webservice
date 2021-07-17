4a7258747690b66dbc3056a202c1806dd0e76547
cndqjacndqja <cndqjacndqja@gmail.com>
2021-07-17 09:44:26 +0900

    init

---
+++ src/main/java/com/example/springproject/SpringBootWebService.java
@@ -0,0 +1,11 @@
+package com.example.springproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebService {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootWebService.class, args);
+    }
+}
