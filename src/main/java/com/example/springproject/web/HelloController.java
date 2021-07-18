a9b3e44ca3cd62a1d249e754aedc0705af47250a
cndqjacndqja <cndqjacndqja@gmail.com>
2021-07-18 10:24:48 +0900

    Add HelloControllerTest

--- src/main/java/com/example/springproject/web/HelloController.java
+++ src/main/java/com/example/springproject/web/HelloController.java
@@ -5,8 +5,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-    @GetMapping("/helllo")
-    public String hello(){
+    @GetMapping("/hello")
+    public String hello() {
         return "hello";
     }
 }
