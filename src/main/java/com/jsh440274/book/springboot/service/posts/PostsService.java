97874816e172940c0bdf6d583698ae2bfb5d35db
jsh <jsh440274>
2021-07-27 21:29:37 +0900

    API관련 내용 추가(PostsApiController, PostsService, PostsSaveRequestDto)

---
+++ src/main/java/com/jsh440274/book/springboot/service/posts/PostsService.java
@@ -0,0 +1,18 @@
+package com.jsh440274.book.springboot.service.posts;
+
+import com.jsh440274.book.springboot.domain.posts.PostsRepository;
+import com.jsh440274.book.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto){
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
