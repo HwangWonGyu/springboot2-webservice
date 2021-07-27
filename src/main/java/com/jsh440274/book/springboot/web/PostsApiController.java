f20142e580554fc5352681e8a11760698769a813
jsh <jsh440274>
2021-07-27 22:46:35 +0900

    API관련 update Test 추가(Posts, PostsService, PostsApiController, PostsApiControllerTest 수정, PostsResponseDto, PostsUpdateRequestDto 추가)

--- src/main/java/com/jsh440274/book/springboot/web/PostsApiController.java
+++ src/main/java/com/jsh440274/book/springboot/web/PostsApiController.java
@@ -1,11 +1,11 @@
 package com.jsh440274.book.springboot.web;
 
 import com.jsh440274.book.springboot.service.posts.PostsService;
+import com.jsh440274.book.springboot.web.dto.PostsResponseDto;
 import com.jsh440274.book.springboot.web.dto.PostsSaveRequestDto;
+import com.jsh440274.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -15,5 +15,13 @@ public class PostsApiController {
     @PostMapping("/api/v1/posts")
     public Long save(@RequestBody PostsSaveRequestDto requestDto){
         return postsService.save(requestDto);
+    }
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto){
+        return postsService.update(id, requestDto);
+    }
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById(@PathVariable Long id){
+        return postsService.findById(id);
     }
 }
