4585d8e0e94fcf867e1cccd1e0336092b60aba72
jsh <jsh440274>
2021-08-09 23:06:06 +0900

    머스태치 설정 및 테스트(build.gradle 디펜던시 추가, index.mustache, IndexController, IndexControllerTest 추가)

---
+++ src/main/java/com/jsh440274/book/springboot/web/IndexController.java
@@ -0,0 +1,15 @@
+package com.jsh440274.book.springboot.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    //머스태치 스타터가 자동으로 src/main/resources/templates/index.mustache로 반환해줌.
+    //앞에있는 디폴트 경로 + 리턴값 + .mustache확장자
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+}
