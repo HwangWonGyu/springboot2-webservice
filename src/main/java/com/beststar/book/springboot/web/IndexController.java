8e47e5c7d4172be2bacad7deb66b9113e98d89ed
HwangWonGyu <hwksdream@naver.com>
2021-08-10 01:58:40 +0900

    feat : add index, POST save and layout

--- src/main/java/com/beststar/book/springboot/web/IndexController.java
+++ src/main/java/com/beststar/book/springboot/web/IndexController.java
@@ -18,4 +18,9 @@ public class IndexController {
     public String index() {
         return "index";
     }
+
+    @GetMapping("/posts/save")
+    public String postsSave() {
+        return "posts-save";
+    }
 }
