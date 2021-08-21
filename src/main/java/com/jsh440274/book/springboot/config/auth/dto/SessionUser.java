fb19515065383f007d34f779d2f42fb2547bf90b
jsh <jsh440274>
2021-08-21 19:30:45 +0900

    로그인 테스트 추가

--- src/main/java/com/jsh440274/book/springboot/config/auth/dto/SessionUser.java
+++ src/main/java/com/jsh440274/book/springboot/config/auth/dto/SessionUser.java
@@ -1,6 +1,7 @@
 package com.jsh440274.book.springboot.config.auth.dto;
 
 import com.jsh440274.book.springboot.domain.user.User;
+import lombok.Getter;
 
 import java.io.Serializable;
 
@@ -8,6 +9,7 @@ import java.io.Serializable;
 //세션에 User클래스를 저장하려고 하면 직렬화가 안되어 있어 불가능하게 됨.
 //그렇다고 직렬화해주는 코드를 User클래스에 넣게 되면 엔티티인 User클래스와 연관된 모든 엔티티들이 직렬화 대상이 되어버림
 //확장성이 구려질 수 있기 때문에 직렬화 기능을 가진 세션 Dto를 새로 만드는 것이 좋다.
+@Getter
 public class SessionUser implements Serializable {
     private String name;
     private String email;
--- src/main/java/com/jsh440274/book/springboot/web/IndexController.java
+++ src/main/java/com/jsh440274/book/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.jsh440274.book.springboot.web;
 
+import com.jsh440274.book.springboot.config.auth.dto.SessionUser;
 import com.jsh440274.book.springboot.service.posts.PostsService;
 import com.jsh440274.book.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,17 +9,24 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     //머스태치 스타터가 자동으로 src/main/resources/templates/index.mustache로 반환해줌.
     //앞에있는 디폴트 경로 + 리턴값 + .mustache확장자
     @GetMapping("/")
     public String index(Model model){
         model.addAttribute("posts", postsService.findAllDesc());
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if(user != null){
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
