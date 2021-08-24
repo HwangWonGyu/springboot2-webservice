16c6ee46029dee552a16c11463bece768f2fc013
jsh <jsh440274>
2021-08-24 23:13:08 +0900

    세션리스, Id의 타입 관련 주석 추가

--- src/main/java/com/jsh440274/book/springboot/config/auth/CustomOAuth2UserService.java
+++ src/main/java/com/jsh440274/book/springboot/config/auth/CustomOAuth2UserService.java
@@ -44,6 +44,8 @@ public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequ
         //User클래스를 사용하지 않고 새로 만들어서 사용한다.
         /*
         세션을 사용하는 경우와 아닌 경우의 차이
+        세션의 경우 브라우저가 종료될 때 까지만 유지 되는 정보를 가지고 있다. 일반적으로 사용자가 브라우저에 머물러 있을 때만 정보를 가지고 있어야 하는 로그인의 특성 상 자주 사용 됨.
+        세션리스의 경우는 브라우저에 머물러 있을 때만 가지고 있어야 하는 정보가 아닌 경우, 고정적인 정보를 화면에 띄워주게 되는 경우에 사용할 수 있다.
          */
         httpSession.setAttribute("user", new SessionUser(user));
 
--- src/main/java/com/jsh440274/book/springboot/domain/user/User.java
+++ src/main/java/com/jsh440274/book/springboot/domain/user/User.java
@@ -13,6 +13,11 @@ import javax.persistence.*;
 public class User extends BaseTimeEntity {
 
     //왜 그냥 long이 아니라 Long일까?
+    /*
+    id의 타입이 long일 경우 0으로 초기화되어 실제 0의 값인지 초기화된 값인지를 구분하기가 힘들어진다.
+    Long의 wrapper타입으로 선언해주면 Nullable하게 명시할 수 있어 0을 구분하기 용이하다.
+    https://hch4102.tistory.com/106
+    */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -32,21 +37,21 @@ public class User extends BaseTimeEntity {
     private Role role;
 
     @Builder
-    public User(String name, String email, String picture, Role role){
+    public User(String name, String email, String picture, Role role) {
         this.name = name;
         this.email = email;
         this.picture = picture;
         this.role = role;
     }
 
-    public User update(String name, String picture){
+    public User update(String name, String picture) {
         this.name = name;
         this.picture = picture;
 
         return this;
     }
 
-    public String getRoleKey(){
+    public String getRoleKey() {
         return this.role.getKey();
     }
 }
