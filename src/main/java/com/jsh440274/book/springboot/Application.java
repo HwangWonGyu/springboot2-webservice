4266be9fd957c5b1e0d6a027fbc7338aad9fea05
jsh <jsh440274>
2021-09-05 12:12:15 +0900

    테스트시 고려사항들 수정.(소셜로그인 관련 설정값들 임의 등록(application.properties, PostsApiControllerTest수정), SecurityConfig사요하지 않는 곳 제외(HelloResponseDtoTest, HelloControllerTest 수정), JpaAuditing이 WebMvcTest에서도 사용되는 문제(JpaConfig 추가, Application 수정))

--- src/main/java/com/jsh440274/book/springboot/Application.java
+++ src/main/java/com/jsh440274/book/springboot/Application.java
@@ -7,7 +7,10 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 //스프링 부트의 자동 설정, 스프링 Bean 읽기와 생성을 모두 자동으로 설정.
 //@SpringBootApplication이 있는 위치부터 설정을 읽어나가기 때문에 이 클래스는 항상 프로젝트의 최상단에 위치해야한다.
 
-@EnableJpaAuditing//JPA Auditing활성화
+//@EnableJpaAuditing이 @SpringBootApplication과 함께 있기 때문에 @WebMvcTest에서도 스캔하게 된다.
+//@EnableJpaAuditing을 사용하기 위해서 @Entity가 필요하기 때문에 이를 테스트에서는 사용하기 어렵다.
+//별도의 JpaConfig파일을 만들어 사용한다.
+//@EnableJpaAuditing//JPA Auditing활성화
 @SpringBootApplication
 public class Application {
     public static void main(String[] args){
--- src/test/java/com/jsh440274/book/springboot/web/HelloControllerTest.java
+++ src/test/java/com/jsh440274/book/springboot/web/HelloControllerTest.java
@@ -1,12 +1,17 @@
 package com.jsh440274.book.springboot.web;
 
+import com.jsh440274.book.springboot.config.auth.SecurityConfig;
 import com.jsh440274.book.springboot.web.HelloController;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -17,7 +22,13 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 //여러 테스트 어노테이션 중 웹(Spring MVC)에 집중할 수 있는 어노테이션
 //사용할 경우 @Controller, @ControllerAdvice 사용 가능, @Service, @Component, @Repository등 사용 불가능.
 @RunWith(SpringRunner.class)
-@WebMvcTest(controllers = HelloController.class)
+//@Service, @Component, @Repository는 스캔대상이 아니기 때문에
+//SecurityConfig를 스캔하지만 이를 생성하는데 필요한 CustomOAuth2UserService는 읽지 않는다.
+//여기서는 SecurityConfig를 사용할 필요가 없기 때문에 제외해줌.
+@WebMvcTest(controllers = HelloController.class,
+        excludeFilters = {
+                @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)
+        })
 public class HelloControllerTest {
 
     //스프링이 관리하는 빈 자동 주입
@@ -27,20 +38,21 @@ public class HelloControllerTest {
     @Autowired
     private MockMvc mvc;
 
+    @WithMockUser(roles = "USER")
     @Test
-    public void is_hello_returned() throws Exception{
+    public void hello가_리턴된다() throws Exception {
         String hello = "hello";
 
         //MockMvc를 통해 /hello주소로 HTTP GET 요청
         mvc.perform(get("/hello"))
                 .andExpect(status().isOk())
                 .andExpect(content().string(hello));
-                //mvc.perform의 결과 검증
-                //HTTP Header의 Status를 검증한다.
-                //200, 404, 500 등의 숫자로 나타나는 Status검증
-                //isOK이므로 200인 경우를 뜻한다.
-                //mvc.perform의 결과 검증
-                //응답된 본문의 내용을 검증한다.
-                //Controller에서 "hello" 값을 리턴하는지 검증한다.
+        //mvc.perform의 결과 검증
+        //HTTP Header의 Status를 검증한다.
+        //200, 404, 500 등의 숫자로 나타나는 Status검증
+        //isOK이므로 200인 경우를 뜻한다.
+        //mvc.perform의 결과 검증
+        //응답된 본문의 내용을 검증한다.
+        //Controller에서 "hello" 값을 리턴하는지 검증한다.
     }
 }
