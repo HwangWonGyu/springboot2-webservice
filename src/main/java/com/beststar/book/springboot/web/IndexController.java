2cbc8d34a805e19fd8efe9f2039a809dfd4e6c97
HwangWonGyu <hwksdream@naver.com>
2021-09-08 21:58:21 +0900

    feat : add login security with oauth 2.0

--- src/main/java/com/beststar/book/springboot/web/IndexController.java
+++ src/main/java/com/beststar/book/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.beststar.book.springboot.web;
 
+import com.beststar.book.springboot.config.auth.LoginUser;
 import com.beststar.book.springboot.config.auth.dto.SessionUser;
 import com.beststar.book.springboot.service.posts.PostsService;
 import com.beststar.book.springboot.web.dto.PostsResponseDto;
@@ -27,7 +28,7 @@ public class IndexController {
     // View Resolver가 처리하게 됨
     // * View Resolver는 URL 요청의 결과를 전달할 타입과 값을 지정하는 관리자 격으로 볼 수 있음
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         // Model
         // 서버 템플릿 엔진에서 사용할 수 있는 객체를 저장할 수 있음
         // 여기서는 postsService.findAllDesc()로 가져온 결과를 posts로 index.mustache에 전달
@@ -37,7 +38,10 @@ public class IndexController {
         // (SessionUser) httpSession.getAttribute("user")
         // 앞서 작성된 CustomOAuth2UserService에서 로그인 성공 시 세션에 SessionUser를 저장하도록 구성
         // 즉, 로그인 성공 시 httpSession.getAttribute("user")에서 값을 가져올 수 있음
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        // SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        // -> @LoginUser SessionUser user 파라미터로 개선됨
+        // 이제는 어느 컨트롤러든지 @LoginUser만 사용하면 세션 정보를 가져올 수 있게 됐음
+
         if (user != null) {
             // index.mustache에서 userName을 사용할 수 있게 model에 저장
             // 세션에 저장된 값이 있을 때만 model에 userName으로 등록
--- src/test/java/com/beststar/book/springboot/web/PostsApiControllerTest.java
+++ src/test/java/com/beststar/book/springboot/web/PostsApiControllerTest.java
@@ -4,24 +4,34 @@ import com.beststar.book.springboot.domain.posts.Posts;
 import com.beststar.book.springboot.domain.posts.PostsRepository;
 import com.beststar.book.springboot.web.dto.PostsSaveRequestDto;
 import com.beststar.book.springboot.web.dto.PostsUpdateRequestDto;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 // @WebMvcTest를 사용하지 않았는데, 이는 JPA 기능이 작동하지 않기 때문
 // Controller와 ControllerAdvice 등 외부 연동과 관련된 부분만 활성화되니
 // 지금처럼 JPA 기능까지 한번에 테스트할 때는 @SpringBootTest와 TestRestTemplate을 사용하면 됨
+@ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class PostsApiControllerTest {
 
@@ -34,12 +44,29 @@ public class PostsApiControllerTest {
     @Autowired
     private PostsRepository postsRepository;
 
+    @Autowired
+    private WebApplicationContext context;
+
+    private MockMvc mvc;
+
+    @BeforeEach
+    public void setup() {
+        mvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .apply(springSecurity())
+                .build();
+    }
+
     @AfterEach
     public void tearDown() throws Exception {
         postsRepository.deleteAll();
     }
 
     @Test
+    @WithMockUser(roles="USER")
+    // @WithMockUser(roles="USER")
+    // 임의의 사용자 인증 추가. 즉, ROLE_USER 권한을 가진 사용자가 API를 요청하는 것과 동일한 효과
+    // 단, MockMvc에서만 작동함
     public void Posts_등록된다() throws Exception {
         //given
         String title = "title";
@@ -53,17 +80,19 @@ public class PostsApiControllerTest {
         String url = "http://localhost:" + port + "/api/v1/posts";
 
         //when
-        ResponseEntity<Long> responseEntity = restTemplate.postForEntity(url, requestDto, Long.class);
+        mvc.perform(post(url)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(new ObjectMapper().writeValueAsString(requestDto)))
+                .andExpect(status().isOk());
 
         //then
-        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
-        assertThat(responseEntity.getBody()).isGreaterThan(0L);
         List<Posts> all = postsRepository.findAll();
         assertThat(all.get(0).getTitle()).isEqualTo(title);
         assertThat(all.get(0).getContent()).isEqualTo(content);
     }
 
     @Test
+    @WithMockUser(roles="USER")
     public void Posts_수정된다() throws Exception {
         //given
         Posts savedPosts = postsRepository.save(Posts.builder().
@@ -87,12 +116,12 @@ public class PostsApiControllerTest {
         HttpEntity<PostsUpdateRequestDto> requestEntity = new HttpEntity<>(requestDto);
 
         //when
-        ResponseEntity<Long> responseEntity = restTemplate.exchange(url, HttpMethod.PUT, requestEntity, Long.class);
+        mvc.perform(put(url)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(new ObjectMapper().writeValueAsString(requestDto)))
+                .andExpect(status().isOk());
 
         //then
-        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
-        assertThat(responseEntity.getBody()).isGreaterThan(0L);
-
         List<Posts> all = postsRepository.findAll();
         assertThat(all.get(0).getTitle()).isEqualTo(expectedTitle);
         assertThat(all.get(0).getContent()).isEqualTo(expectedContent);
